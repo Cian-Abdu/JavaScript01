3e8d55130741d648d3a4e81469d0009661039bc9
Hendel Yoshua Shimizu Nakashima <32624936+Cian-Abdu@users.noreply.github.com>
2022-03-28 22:49:50 -0300

    Add files via upload

---
+++ 2-Var.js
@@ -0,0 +1,22 @@
+const idade = 26;
+const idadeSomada = idade + 2;
+const idadeSubtracao = idade - 2;
+const idadeMultiplicacao = idade * 2;
+const idadeDivisao = idade / 2;
+
+console.log("Trabalhando com Variaveis:");
+
+console.log(" ")
+
+console.log("Idade inicial:", idade);
+console.log("Idade Adicao:", idade + 2);
+console.log("Idade Subtracao:", idade - 2);
+console.log("Idade Multiplicacao", idade * 2);
+console.log("Idade Divisao:", idade / 2);
+
+console.log(" ")
+
+console.log("Idade Adicao com Variavel:", idadeSomada);
+console.log("Idade Subtracao com Variavel:", idadeSubtracao);
+console.log("Idade Multiplicacao com Variavel:", idadeMultiplicacao);
+console.log("Idade Divisao com Variavel:", idadeDivisao);
